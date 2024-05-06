09fd4b01efbe1ebf65aa8433064f80259c603b9a
Anahit Grigoryan1 <agrigory15@pmintl.net>
2024-05-06 22:22:54 +0400

    api tests

--- playwright.config.js
+++ playwright.config.js
@@ -12,17 +12,21 @@ import { testPlanFilter } from 'allure-playwright/dist/testplan';
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: 'src/tests/specs',
+  testDir: 'src/tests',
   testMatch: '**/**/**/*spec.js',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 2,
+  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : 2,
   grep: testPlanFilter(),
   reporter: 'allure-playwright',
   use: {
     trace: 'on-first-retry',
-    screenshot: 'only-on-failure'
+    screenshot: 'only-on-failure',
+    extraHTTPHeaders: {
+      'Content-Type': 'application/json',
+      'Authorization': `bearer ${process.env.BEARER_TOKEN}`
+    },
   },
   projects: [
     {
@@ -30,9 +34,9 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'], channel: 'chrome' }
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
   ]
 });
